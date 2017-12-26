8af0e730264b6d45b61a58b9c0482eaf39e801ce
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-26 19:05:50 +0100

    open and close connection to database with application context

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -6,3 +6,6 @@ app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('flaskblog.configs.DevelopmentConfig') # default config
 app.config.from_pyfile('config.py', silent=True) # override default config with file in instance folder
 app.config.from_envvar('CONFIG', silent=True) # override default config with file in envvar CONFIG
+
+# database
+import flaskblog.db_manager
--- flaskblog/db_manager.py
+++ flaskblog/db_manager.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+from flask import g
 from flaskblog import app
 
 def connect_db():
@@ -12,5 +13,17 @@ def connect_db():
 
     # establish connection
     con = sqlite3.connect(db)
-    con.row_factory = sqlite3.Row # index based and case-insensitive name-based access to columns with no overhead
+    con.row_factory = sqlite3.Row  # index based and case-insensitive name-based access to columns with no overhead
     return con
+
+def get_db():
+    """opens a database connection if there is none yet for the current application context"""
+    if not hasattr(g, 'db'):
+        g.db = connect_db()
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    """closes database connection at the end of a request"""
+    if hasattr(g, 'db'):
+        g.db.close()
