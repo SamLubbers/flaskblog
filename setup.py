4c9f7097589632d1bcfaa722be2585f0c472c073
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-26 10:14:17 +0100

    flask app instance as package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='flaskblog',
+    version='0.0.1',
+    packages=['flaskblog'],
+    description='personal blog developed with Flask and SQlite3',
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
