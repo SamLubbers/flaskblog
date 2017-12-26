0a86476de08dada7e3ec6fe693117e8644ea17f8
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-26 18:36:55 +0100

    database created in location specified in configuration

--- flaskblog/db_manager.py
+++ flaskblog/db_manager.py
@@ -1,8 +1,16 @@
 import sqlite3
+import os
 from flaskblog import app
 
-def connect_db(db):
-    """connect to a specific sqlite3 database"""
+def connect_db():
+    """connect to a sqlite3 database defined in configuration"""
+    # to avoid errors make sure all directories leading to db location exist
+    db = app.config['DATABASE']
+    path_to_db = os.path.dirname(db)
+    if not os.path.exists(path_to_db):
+        os.makedirs(path_to_db)
+
+    # establish connection
     con = sqlite3.connect(db)
     con.row_factory = sqlite3.Row # index based and case-insensitive name-based access to columns with no overhead
     return con
