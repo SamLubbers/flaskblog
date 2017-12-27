b69cc78538b99b022e55877fe1a667efdb9969d4
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-27 10:05:08 +0100

    login view and template

--- flaskblog/views.py
+++ flaskblog/views.py
@@ -1,10 +1,23 @@
 from flaskblog import app
 from flaskblog.db_manager import get_db
-from flask import render_template
+from flask import render_template, request, session, redirect, url_for
 
 @app.route('/')
-def index():
+def blog_entries():
     db = get_db()
     cur = db.execute('select title, text from blogentries order by id desc')
     entries = cur.fetchall()
     return render_template('blog_entries.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form.get('username') != app.config['USERNAME']:
+            error = 'invalid username'
+        elif request.form.get('password') != app.config['PASSWORD']:
+            error = 'invalid password'
+        else:
+            session['logged_in'] = True
+            redirect(url_for('blog_entries'))
+    return render_template('login.html', error=error)
