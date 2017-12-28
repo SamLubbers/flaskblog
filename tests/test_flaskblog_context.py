4e0f3fb07a0f9b8d2c49132c8c1f69cfc5541edb
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-28 16:35:29 +0100

    test request context db connection

--- tests/test_flaskblog_context.py
+++ tests/test_flaskblog_context.py
@@ -1,14 +1,21 @@
-from flask import request
+from flask import request, g
 from flaskblog import app
+from flaskblog.db_manager import get_db
 
 def test_routes():
     with app.test_request_context('/'):
         assert request.path == '/'
 
+def test_db():
+    with app.test_request_context('/'):
+        get_db()
+        assert str(type(g.db)) == '<class \'sqlite3.Connection\'>'
+
 if __name__ == '__main__':
     try:
         test_routes()
+        test_db()
     except AssertionError as err:
         raise Exception('context test error') from err
     else:
-        print('no errors testing request context')
+        print('no errors when testing request context')
