b55ce396c2ce93ebcd39e6bcfda7fff1e90f81c7
Sam~Lubbers <lubbers.sc@gmail.com>
2017-12-28 15:47:21 +0100

    login/logout tests

--- tests/test_flaskblog.py
+++ tests/test_flaskblog.py
@@ -3,6 +3,7 @@ import flaskblog
 import unittest
 import tempfile
 
+
 class FlaskblogTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -15,6 +16,36 @@ class FlaskblogTestCase(unittest.TestCase):
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(flaskblog.app.config['DATABASE'])
+
+    def test_empty_db(self):
+        res = self.app.get('/')
+        assert b'no entries to show' in res.data
+
+    def login(self, username, password):
+        return self.app.post('/login',
+                             data=dict(username=username,
+                                       password=password),
+                             follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_login_logout(self):
+        # correct login
+        res = self.login('admin', 'default')
+        assert b'log out' in res.data
+        assert b'New blog' in res.data
+        # correct log out
+        res = self.logout()
+        print(res.data)
+        assert b'log in' in res.data
+        # login wrong username
+        res = self.login('adminn', 'default')
+        assert b'invalid username' in res.data
+        # login wrong password
+        self.logout()
+        res = self.login('admin', 'defaul')
+        assert b'invalid password' in res.data
 
 if __name__ == '__main__':
     unittest.main()
