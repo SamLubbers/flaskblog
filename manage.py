23ac835458e784d58025d85d2f31027f39334966
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-17 06:47:59 +0000

    added support for flask migrate

--- manage.py
+++ manage.py
@@ -1,12 +1,18 @@
 from flask_script import Manager
 from flaskblog import create_app
+from flask_migrate import Migrate, MigrateCommand
+from flaskblog.models import db
 
 app = create_app()
 manager = Manager(app)
+
+# db migrate
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 @manager.command
 def run():
     app.run()
 
 if __name__ == '__main__':
-    run() # direct call to run so we can run and debug from PyCharm
+    manager.run() # direct call to run so we can run and debug from PyCharm
