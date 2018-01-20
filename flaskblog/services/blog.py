47951dfb781ef4fbaf19c963fae2eb0862d26e3e
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-20 11:16:14 +0000

    created cach_view decorator

--- flaskblog/services/blog.py
+++ flaskblog/services/blog.py
@@ -3,7 +3,8 @@ from flaskblog.models import Blog, db
 from werkzeug.contrib.cache import SimpleCache
 
 cache = SimpleCache()
-five_minutes = 60 * 5 # default caching time is set to five minutes
+five_minutes = 60 * 5  # default caching timeout is set to five minutes
+
 
 def get_blog(blog_id):
     blog = cache.get(blog_id)
@@ -12,11 +13,13 @@ def get_blog(blog_id):
         cache.set(blog_id, blog, timeout=five_minutes)
     return blog
 
+
 def insert_blog(blog_title, blog_text):
     if blog_title and blog_text:
         blog = Blog(title=blog_title, text=blog_text)
         db.session.add(blog)
         db.session.commit()
+
 
 def get_all_blogs():
     blogs = cache.get('all_blogs')
