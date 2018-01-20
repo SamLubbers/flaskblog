498c820e8d0cf688c24ec1d1af35f84553f64075
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-20 10:07:00 +0000

    refactored blog insert to services package

--- flaskblog/services/blog.py
+++ flaskblog/services/blog.py
@@ -1,13 +1,20 @@
-from flaskblog.models import Blog
+"""services module for interacting with the Blog model"""
+from flaskblog.models import Blog, db
 from werkzeug.contrib.cache import SimpleCache
 
-c = SimpleCache()
+cache = SimpleCache()
 five_minutes = 60 * 5 # default caching time is set to five minutes
 
 def get_blog(blog_id):
-    blog = c.get(blog_id)
+    blog = cache.get(blog_id)
     if blog is None:
         print('blog is loaded from db')
         blog = Blog.query.filter_by(id=blog_id).one()
-        c.set(blog_id, blog, timeout=five_minutes)
+        cache.set(blog_id, blog, timeout=five_minutes)
     return blog
+
+def insert_blog(blog_title, blog_text):
+    if blog_title and blog_text:
+        blog = Blog(title=blog_title, text=blog_text)
+        db.session.add(blog)
+        db.session.commit()
--- flaskblog/views/blog.py
+++ flaskblog/views/blog.py
@@ -1,25 +1,20 @@
-from flask.blueprints import Blueprint
-from flaskblog.models import db
-from flaskblog.models import Blog
 from flask import render_template, request, redirect, url_for
+from flask.blueprints import Blueprint
 from flaskblog import services
 
 bp = Blueprint('blog', __name__, url_prefix='/blog')
 
+
 @bp.route('/new', methods=['GET', 'POST'])
 def new_blog():
     if request.method == 'POST':
-        blog_title = request.form.get('title')
-        blog_text = request.form.get('text')
-        if blog_title and blog_text: # make sure they are not null, later change this to the client side
-            blog = Blog(title=blog_title,text=blog_text)
-            db.session.add(blog)
-            db.session.commit()
-            return redirect(url_for('index.index'))
+        services.insert_blog(blog_title=request.form.get('title'),
+                             blog_text=request.form.get('text'))
+        return redirect(url_for('index.index'))
     return render_template('blog/new_blog.html')
+
 
 @bp.route('/<int:blog_id>')
 def view_blog(blog_id):
     blog = services.get_blog(blog_id)
-    return render_template('blog/view_blog.html',blog=blog)
-
+    return render_template('blog/view_blog.html', blog=blog)
