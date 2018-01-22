6bfec5ea0df57934bf5d96776c97da0ccd50346b
Sam~Lubbers <lubbers.sc@gmail.com>
2018-01-22 07:58:18 +0000

    created API endpoint to get specific blog

--- flaskblog/views/api.py
+++ flaskblog/views/api.py
@@ -1,10 +1,22 @@
-from flask import jsonify
+from flask import jsonify, abort
 from flask.blueprints import Blueprint
-from flaskblog.services import get_all_blogs
+from flaskblog.services import get_all_blogs, get_blog
+from sqlalchemy.orm.exc import NoResultFound
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
 @bp.route('/allblogs')
 def allblogs():
+    # TODO exception handling if no blogs exist
     return jsonify(all_blogs=[blog.serialize for blog in get_all_blogs()])
+
+
+@bp.route('/blog/<int:blog_id>')
+def blog(blog_id):
+    try:
+        blog = get_blog(blog_id)
+    except NoResultFound:
+        # TODO return 404 without html
+        return abort(404)
+    return jsonify(blog=blog.serialize)
