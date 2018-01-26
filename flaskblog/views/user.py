0fd11400869e3e0768a2c946f746e7b32b1504f3
SamLubbers <lubbers.sc@gmail.com>
2018-01-26 08:15:23 +0000

    completed signup view. Users can now signup to the system

--- flaskblog/views/user.py
+++ flaskblog/views/user.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 from flaskblog.forms import SignUpForm, SignInForm
+from flaskblog.services.user import create_user
 
 bp = Blueprint('user', __name__)
 
@@ -9,10 +10,16 @@ bp = Blueprint('user', __name__)
 def signup():
     form = SignUpForm(request.form)
     if request.method == 'POST' and form.validate():
-        # TODO call to register user function in services
-        return redirect(url_for('index.index'))
+        if create_user(form.username.data, form.password.data):
+            flash('you have succesfully created a new account',
+                  category='success')
+            return redirect(url_for('index.index'))
+        else:
+            flash('username already exists', category='danger')
+            return redirect(url_for('user.signup'))
 
     return render_template('user/signup.html', form=form)
+
 
 @bp.route('/signin', methods=['GET', 'POST'])
 def signin():
