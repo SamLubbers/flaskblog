5fa7e45a4d852334993870d87d13fe14c2343dd5
SamLubbers <lubbers.sc@gmail.com>
2018-01-26 10:52:06 +0000

    api 404 returns friendly response in json format

--- flaskblog/views/api.py
+++ flaskblog/views/api.py
@@ -1,15 +1,19 @@
-from flask import jsonify, abort
+from flask import jsonify, abort, make_response
 from flask.blueprints import Blueprint
-from flaskblog.services import get_all_blogs, get_blog
 from sqlalchemy.orm.exc import NoResultFound
+
+from flaskblog.services import get_all_blogs, get_blog
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
 
 @bp.route('/allblogs')
 def allblogs():
-    # TODO exception handling if no blogs exist
-    return jsonify(all_blogs=[blog.serialize for blog in get_all_blogs()])
+    all_blogs = [blog.serialize for blog in get_all_blogs()]
+    if not all_blogs:
+        abort(404)
+    else:
+        return jsonify(all_blogs=all_blogs)
 
 
 @bp.route('/blog/<int:blog_id>')
@@ -17,6 +21,10 @@ def blog(blog_id):
     try:
         blog = get_blog(blog_id)
     except NoResultFound:
-        # TODO return 404 without html
         return abort(404)
     return jsonify(blog=blog.serialize)
+
+
+@bp.errorhandler(404)
+def api_404(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
--- manage.py
+++ manage.py
@@ -14,12 +14,5 @@ manager.add_command('db', MigrateCommand)
 def run():
     app.run()
 
-@manager.command
-def test():
-    import unittest
-
-    testsuite = unittest.TestLoader().discover(start_dir='tests/')
-    unittest.TextTestRunner(verbosity=1).run(testsuite)
-
 if __name__ == '__main__':
     manager.run()
