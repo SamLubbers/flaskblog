d2268e6dac58aa589797142b125af3b8a0097ba7
SamLubbers <lubbers.sc@gmail.com>
2018-01-26 08:25:23 +0000

    added flask-login required methods to user

--- flaskblog/models/users.py
+++ flaskblog/models/users.py
@@ -10,3 +10,15 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User({self.username})"
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.email)
